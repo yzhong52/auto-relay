35fef9faa1d8c515ee873125ffe45cc46bca72bb
Yuchen Zhong <yzhong.cs@gmail.com>
2026-04-23 22:08:39 -0700

    Bump version to 1.2.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.autorelay.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
